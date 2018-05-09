8117272d40e82a9f5456319c80603c341c7e9bdd
hitesh-92 <hiteshgohil@hotmail.co.uk>
2018-05-09 19:06:20 +0100

    deck cards and counter

--- deck.py
+++ deck.py
@@ -1,19 +1,47 @@
-def Deck():
+from random import randint
+
+class Deck():
     """
     Deck of cards, containing 52 cards with 4 suits containing 2-A
     """
 
-    suits = ['H','D','S','C']
-    values = [2,3,4,5,6,7,8,9,10,'J','Q','K','A']
-
-
     def __init__(self):
-        currentCards = []
+        self.currentCards = []
+
 
     def newDeck(self):
-        for i in range(len(suits)):
-            print(i)
+        """
+        Set up new deck of cards with all 52 cards. Appends to currentCards
+        """
+        suits = ['H','D','S','C']
+        values = [2,3,4,5,6,7,8,9,10,'Jack','Queen','King','Ace']
+
+        for suit in suits:
+            #Prints 4 suits letters
+            # print(suit)
+
+            for value in values:
+                #Prints all values with each suit
+                # print(f"{suit} {value}")
+                self.currentCards.append(f"{suit} {value}")
+
+    def cardCount(self):
+        return len(self.currentCards)
+
+
+
+
+
 
 deck = Deck()
+deck.newDeck()
 
-print(deck.currentCards)
+#Prints cards currently in deck
+# print(deck.currentCards)
+
+#Print amount of cards currently in deck
+# print(deck.cardCount())
+
+#Pick a random card
+
+print(randint(0,9))
